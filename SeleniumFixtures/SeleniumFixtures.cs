a32956c7be26f6af52bf57516dc15da7f4dedc32
Prash <prashanth.gs@gmail.com>
2019-08-27 22:15:20 -0500

    second commit

---
+++ SeleniumFixtures/SeleniumFixtures.cs
@@ -0,0 +1,41 @@
+﻿using fitlibrary;
+using fit.Runner;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using SeleniumFixtures.Pages;
+
+namespace SeleniumFixtures
+{
+    public class WebFixture
+    {
+        public WebFixture()
+        {
+        }
+
+        public void OpenUrl(string url)
+        {
+            Browser.Init();
+            Browser.Driver.Navigate().GoToUrl(url);
+        }
+
+        public void Search(string text)
+        {
+            SearchPage googlePage = new SearchPage();
+            googlePage.SearchText("Fitnesse");
+        }
+
+        public void CloseBrowser()
+        {
+            if (Browser.Driver!= null)
+            {
+                Browser.Driver.Dispose();
+            }
+        }
+    }
+}
---
+++ fitnesseRun.cmd
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+java -jar fitnesse-standalone.jar -p 1010 -d "FitnesseFixtures" -r fitNesseRoot
+=======
+java -jar fitnesse-standalone.jar -p 8080 -d "C:\Users\prash\source\repos\SeleniumFixtures" -r fitNesseRoot
+>>>>>>> parent of 73e045e... Updated JsonRestApiFixture
+=======
+java -jar fitnesse-standalone.jar -p 8080 -d "C:\Users\prash\source\repos\SeleniumFixtures" -r fitNesseRoot
+>>>>>>> parent of 73e045e... Updated JsonRestApiFixture
+=======
+java -jar fitnesse-standalone.jar -p 8080 -d "C:\Users\prash\source\repos\SeleniumFixtures" -r fitNesseRoot
+>>>>>>> parent of 73e045e... Updated JsonRestApiFixture
+=======
+java -jar fitnesse-standalone.jar -p 8080 -d "C:\Users\prash\source\repos\SeleniumFixtures" -r fitNesseRoot
+>>>>>>> parent of 73e045e... Updated JsonRestApiFixture
+=======
+java -jar fitnesse-standalone.jar -p 8080 -d "C:\Users\prash\source\repos\SeleniumFixtures" -r fitNesseRoot
+>>>>>>> parent of 73e045e... Updated JsonRestApiFixture
+pause
